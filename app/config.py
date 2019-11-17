a626c04feb571f9822e71c889e004e4627845d87
osman67239121@gmail.com <osman67239121@gmail.com>
2019-11-18 00:10:32 +0300

    adds configuration class,stores api key

---
+++ app/config.py
@@ -0,0 +1,30 @@
+class Config:
+
+
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?&apiKey={}'
+    '''
+    General configuration parent class
+    '''
+    pass
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
--- app/views.py
+++ app/views.py
@@ -4,5 +4,10 @@ from app import app
 @app.route('/')
 def index():
 
+    title = 'home, welcome to news highlights'
+    return render_template('index.html', title = title)
+@app.route('/news/<int:news_id>')
+def news(news_id):
 
-    return render_template('index.html')
+
+    return render_template('news.html', id = news_id)
